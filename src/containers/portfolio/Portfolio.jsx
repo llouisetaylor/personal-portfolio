c778eaa626bd99686c7f9b8cd93b3484190bd6c4
Louise <taylor.louise.ann@gmail.com>
2018-03-23 12:10:44 +0000

    Update CV to use correct dates lol

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -2,96 +2,75 @@ import React from 'react';
 
 import '../../styles/containers/portfolio/portfolio.css';
 
-import likeSkin from '../../assets/likeskin.jpg';
-import make from '../../assets/make.jpg';
-import electroHero from '../../assets/electro-hero.jpg';
-import beeHappy from '../../assets/beehappy.jpg';
-import doughUniverse from '../../assets/doughuniverse.jpg';
+import data from './data.js';
+
+const getProfessionalProjects = () => {
+
+    return data.professional.map((project) => {
+
+        const description = project.description.map((listItem) => {
+            return (
+                <li>{ listItem }</li>
+            );
+        })
+
+        return (
+            <div className="portfolio__item">
+                <a href={ project.url } target="blank">
+                    <div className="portfolio__overlay">
+                        <ul>
+                            { description }
+                        </ul>
+                    </div>
+                    <img
+                        className="portfolio__image"
+                        src={ project.image.url }
+                        alt={ project.image.alt }
+                    />
+                </a>
+            </div>
+        )
+    })
+}
+
+const getPersonalProjects = () => {
+
+    return data.personal.map((project) => {
+
+        const description = project.description.map((listItem) => {
+            return (
+                <li>{ listItem }</li>
+            );
+        })
+
+        return (
+            <div className="portfolio__item">
+                <a href={ project.url } target="blank">
+                    <div className="portfolio__overlay">
+                        <ul>
+                            { description }
+                        </ul>
+                    </div>
+                    <img
+                        className="portfolio__image"
+                        src={ project.image.url }
+                        alt={ project.image.alt }
+                    />
+                </a>
+            </div>
+        )
+    })
+}
 
 const Porfolio = () => (
     <div className="portfolio">
-        <h2>Projects I have worked on</h2>
+        <h2>Professional projects</h2>
         <section className="portfolio__projects">
-            <div className="portfolio__item">
-                <a href="https://make.techwillsaveus.com" target="blank">
-                    <div className="portfolio__overlay">
-                        <ul>
-                            <li>Led a project to drive sign-ups on our online learning platform.</li>
-                            <li>This involved a design overhaul, building several experiences including a user sharing feature and support for multiple translations.</li>
-                            <li>Technologies: React, Redux, Sass.</li>
-                        </ul>
-                    </div>
-                    <img
-                        className="portfolio__image"
-                        src={ make }
-                        alt="A screenshot of a website for kits made by Technology Will Save Us featuring lists of kit projects"
-                    />
-                </a>
-            </div>
-            <div className="portfolio__item">
-                <a href="https://play.google.com/store/apps/details?id=com.arnold&hl=en" target="blank">
-                    <div className="portfolio__overlay">
-                        <ul>
-                            <li>Supported the development of a native app.</li>
-                            <li>Technologies: React Native.</li>
-                        </ul>
-                    </div>
-                    <img
-                        className="portfolio__image"
-                        src={ doughUniverse }
-                        alt="A screenshot from the Dough Universe mobile and tablet with playful illustrations of dough balls"
-                    />
-                </a>
-            </div>
-            <div className="portfolio__item">
-                <a href="https://make.techwillsaveus.com/electro-hero-kit/" target="blank">
-                    <div className="portfolio__overlay">
-                        <ul>
-                            <li>Built an Avengers themed toy manual for a Tech Will Save Us X Disney collaboration.</li>
-                            <li>This involved building components with complex custom styling.</li>
-                            <li>Technologies: React and Sass.</li>
-                        </ul>
-                    </div>
-                    <img
-                        className="portfolio__image"
-                        src={ electroHero }
-                        alt="A screenshot of an Iron Man themed toy manual."
-                    />
-                </a>
-            </div>
+            { getProfessionalProjects() }
         </section>
         <h2>Personal projects</h2>
         <section className="portfolio__projects">
-            <div className="portfolio__item">
-                <a href="https://llouisetaylor.github.io/JSToys/bee-happy/" target="blank">
-                    <div className="portfolio__overlay">
-                        <ul>
-                            <li>This web app determines if a text entry is happy, sad or unknown based on a predetermined set of keywords and displays this on a scale.</li>
-                            <li>Technologies: jQuery and VanillaJS.</li>
-                        </ul>
-                    </div>
-                    <img
-                        className="portfolio__image"
-                        src={ beeHappy }
-                        alt="A screenshot of a playful word game for children, with bright colours and a bumble bee character."
-                    />
-                </a>
-            </div>
-            <div className="portfolio__item">
-                <a href="https://mljgreer1.github.io" target="blank">
-                    <div className="portfolio__overlay">
-                        <ul>
-                            <li>Built an online poetry magazine as part of a competition for Code First: Girls.</li>
-                            <li>Technologies: jQuery and VanillaJS.</li>
-                        </ul>
-                    </div>
-                    <img
-                        className="portfolio__image"
-                        src={ likeSkin }
-                        alt="A screenshot of an online poetry magazine with pastel tones and features."
-                    />
-                </a>
-            </div>
+            { getPersonalProjects() }
         </section>
     </div>
 );
---
+++ src/containers/portfolio/data.js
@@ -0,0 +1,63 @@
+module.exports = {
+    professional: [
+        {
+            url: 'https://make.techwillsaveus.com',
+            image: {
+                url: '../../assets/make.jpg',
+                alt: 'A screenshot of a website for kits made by Technology Will Save Us featuring lists of kit projects'
+            },
+            description: [
+                'Led a front-end project to drive sign-ups on our online learning platform.',
+                'This involved a design overhaul, building several experiences including a user sharing feature and support for multiple translations.',
+                'Technologies: React, Redux, Sass.'
+            ]
+        },
+        {
+            url: 'https://play.google.com/store/apps/details?id=com.arnold&hl=en',
+            image: {
+                url: '../../assets/doughuniverse.jpg',
+                alt: 'A screenshot from the Dough Universe mobile and tablet with playful illustrations of dough balls'
+            },
+            description: [
+                'Supported the development of a native app.',
+                'Technologies: React Native.'
+            ]
+        },
+        {
+            url: 'https://make.techwillsaveus.com/electro-hero-kit/',
+            image: {
+                url: '../../assets/electro-hero.jpg',
+                alt: 'A screenshot of an Iron Man themed toy manual.'
+            },
+            description: [
+                'Built an Avengers themed toy manual for a Tech Will Save Us X Disney collaboration.',
+                'This involved building components with complex custom styling.',
+                'Technologies: React and Sass.'
+            ]
+        }
+    ],
+    personal: [
+        {
+            url: 'https://llouisetaylor.github.io/JSToys/bee-happy/',
+            image: {
+                url: '../../assets/beehappy.jpg',
+                alt: 'A screenshot of a playful word game for children, with bright colours and a bumble bee character.'
+            },
+            description: [
+                'This web app determines if a text entry is happy, sad or unknown based on a predetermined set of keywords and displays this on a scale.',
+                'Technologies: jQuery and VanillaJS.'
+            ]
+        },
+        {
+            url: 'https://mljgreer1.github.io',
+            image: {
+                url: '../../assets/likeskin.jpg',
+                alt: 'A screenshot of an online poetry magazine with pastel tones and features.'
+            },
+            description: [
+                'Built an online poetry magazine as part of a competition for Code First: Girls.',
+                'Technologies: jQuery and VanillaJS.'
+            ]
+        }
+    ]
+}
