63d94fb928fb8eef721cbe1b9a138f9c15e48c8b
Louise <taylor.louise.ann@gmail.com>
2018-03-23 12:05:26 +0000

    Update home page subtitle

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -5,7 +5,7 @@ import '../../styles/containers/home/home.css';
 const Home = () => (
     <div className="profile">
         <h1 className="profile__heading">Louise<br/>Taylor</h1>
-        <h2 className="profile__subheading">Junior Developer at Technology Will Save Us</h2>
+        <h2 className="profile__subheading">Junior Front End Software Engineer at BBC</h2>
     </div>
 );
 
