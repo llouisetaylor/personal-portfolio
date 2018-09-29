605b193d64886eff972334f673b4a807984c0ca6
Louise <taylor.louise.ann@gmail.com>
2018-09-29 16:53:33 +0100

    Add codepen and twitter to footer and load svgs the proper way

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
+import SVG from 'react-inlinesvg';
 
 import '../../styles/components/footer/footer.css';
 
 import github from '../../assets/github.svg';
 import medium from '../../assets/medium.svg';
 import linkedin from '../../assets/linkedin.svg';
+import twitter from '../../assets/twitter.svg';
+import codepen from '../../assets/codepen.svg';
 
-const svgStyles = {
-    height: '40px',
-    width: '40px',
-    padding: '10px'
-};
 
 const Footer = () => (
     <div className="footer">
-        <a href="https://github.com/llouisetaylor" target="blank">
-            <img
-                src={ github }
-                alt="The GitHub logo"
-                style={ svgStyles } />
+        <a href="https://github.com/llouisetaylor" target="blank" aria-label="GitHub">
+            <SVG src={ github } className="footer__icon" />
         </a>
-        <a href="https://www.linkedin.com/in/louise-taylor-757a55130/" target="blank">
-            <img
-                src={ linkedin }
-                alt="The LinkedIn logo"
-                style={ svgStyles }
-            />
+        <a href="https://codepen.io/llouisetaylor/" target="blank" aria-label="Codepen">
+            <SVG src={ codepen } className="footer__icon" />
         </a>
-        <a href="https://medium.com/@llouisetaylor" target="blank">
-            <img
-                src={ medium }
-                alt="The Medium logo"
-                style={ svgStyles }
-            />
+        <a href="https://www.linkedin.com/in/louise-taylor-757a55130/" target="blank" aria-label="LinkedIn">
+            <SVG src={ linkedin } className="footer__icon" />
+        </a>
+        <a href="https://medium.com/@llouisetaylor" target="blank" aria-label="Medium">
+            <SVG src={ medium } className="footer__icon" />
+        </a>
+        <a href="https://twitter.com/llouisetaylor" target="blank" aria-label="Twitter">
+            <SVG src={ twitter } className="footer__icon" />
         </a>
     </div>
 );
