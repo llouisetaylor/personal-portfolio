8dab0e4da31a6cd31691aea9aa94f2361cc4c167
Louise <taylor.louise.ann@gmail.com>
2018-10-04 21:49:39 +0100

    Add aria-label and description to portfolio item

--- src/components/portfolio-item/PortfolioItem.jsx
+++ src/components/portfolio-item/PortfolioItem.jsx
@@ -3,13 +3,20 @@ import React from "react";
 import "../../styles/components/portfolio-item/portfolio-item.css";
 
 const PortfolioItem = ({ project }) => (
-  <a href={project.url} target="blank">
+  <a
+    href={ project.url }
+    target="blank"
+    aria-label={ project.name }
+    aria-describedby="portfolio-item-description"
+  >
     <div key={project.url} className="portfolio-item">
       <div className="portfolio-item__overlay">
-        <ul>
-          {project.description.map(listItem => {
-            return <li key={listItem}>{listItem}</li>;
-          })}
+        <ul id="portfolio-item-description">
+          {
+            project.description.map(listItem => {
+              return <li key={ listItem }>{ listItem }</li>
+            })
+          }
         </ul>
       </div>
       <img className="portfolio-item__image" src={project.image} alt="" />
