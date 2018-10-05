4f57b13d85ebc06ce9a4f660f9bb0714eb1cba37
Louise <taylor.louise.ann@gmail.com>
2018-10-05 11:07:11 +0100

    Rename CV to 'work'

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,14 @@ import { Route, Switch } from 'react-router';
 
 import Home from '../containers/home/Home.jsx';
 import Portfolio from '../containers/portfolio/Portfolio.jsx';
-import CurriculumVitae from '../containers/curriculum-vitae/CurriculumVitae.jsx';
+import Work from '../containers/work/Work.jsx';
 import About from '../containers/about/About.jsx';
 
 const routes = (
   <Switch>
     <Route exact path="/" component={ Home } />
     <Route path="/portfolio" component={ Portfolio } />
-    <Route path="/cv" component={ CurriculumVitae } />
+    <Route path="/work" component={ Work } />
     <Route path="/about" component={ About } />
   </Switch>
 );
