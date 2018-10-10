3358c523acb13478e5d06027634de729f9bbb51f
Louise <taylor.louise.ann@gmail.com>
2018-10-10 20:35:26 +0100

    Update portfolio layout

--- src/components/featured-project/FeaturedProject.jsx
+++ src/components/featured-project/FeaturedProject.jsx
@@ -3,31 +3,32 @@ import React from 'react';
 import '../../styles/components/featured-project/featured-project.css';
 
 const FeaturedProject = ({ project }) => (
-  <div className="featured-project">
-    <div className="featured-project__left">
-      <img className="featured-project__image" src={project.image} alt="" />
-    </div>
+  <a href={ project.url }>
+    <div className="featured-project">
+      <div className="featured-project__left">
+        <img className="featured-project__image" src={ project.image } alt="" />
+      </div>
 
-    <div className="featured-project__right">
-      <a href={project.url}>
-        <h3 className="featured-project__title">{project.title}</h3>
-      </a>
+      <div className="featured-project__right">
+        <h3 className="featured-project__title featured-project__title--hide-mobile">{ project.title }</h3>
 
-      <div className="featured-project__description">
-        <ul>
-          {project.description.map(listItem => {
-            return <li key={listItem}>{listItem}</li>;
-          })}
-        </ul>
+        <div className="featured-project__description">
+          <h3 className="featured-project__title featured-project__title--hide-desktop">{ project.title }</h3>
+          <ul>
+            { project.description.map(listItem => {
+              return <li key={ listItem }>{ listItem }</li>;
+            }) }
+          </ul>
 
-        <ul className="featured-project__technologies">
-          {project.technologies.map(technology => {
-            return <li key={technology}>{technology}</li>;
-          })}
-        </ul>
+          <ul className="featured-project__technologies">
+            { project.technologies.map(technology => {
+              return <li key={ technology }>{ technology }</li>;
+            }) }
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 );
 
 export default FeaturedProject;
--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Project from '../../components/project/Project'
-import FeaturedProject from '../../components/featured-project/FeaturedProject'
+import Project from "../../components/project/Project";
+import FeaturedProject from "../../components/featured-project/FeaturedProject";
 
-import '../../styles/containers/portfolio/portfolio.css';
+import "../../styles/containers/portfolio/portfolio.css";
 
-import data from './data.js';
+import data from "./data.js";
 
-const renderFeaturedProjects = (projects) => (
-    projects.map((project) => (
-        <FeaturedProject
-            key={ project.url }
-            project={ project }
-        />
-    ))
-);
+const renderFeaturedProjects = projects =>
+  projects.map(project => (
+    <FeaturedProject key={project.url} project={project} />
+  ));
 
-const renderProjects = (projects) => (
-    projects.map((project) => (
-        <Project
-            key={ project.url }
-            project={ project }
-        />
-    ))
-)
+const renderProjects = projects =>
+  projects.map(project => <Project key={project.url} project={project} />);
 
 const Porfolio = () => (
-    <div className="portfolio">
-        <h2>Featured projects</h2>
-        <section className="portfolio__featured-projects">
-            { renderFeaturedProjects(data.featured) }
-        </section>
-        <h2>More projects</h2>
-        <section className="portfolio__projects">
-            { renderProjects(data.other) }
-        </section>
-    </div>
+  <div className="portfolio">
+    <section className="portfolio__featured-projects">
+      <h2>Featured projects</h2>
+      {renderFeaturedProjects(data.featured)}
+    </section>
+    <section>
+      <h2>More projects</h2>
+      <div className="portfolio__projects">
+        { renderProjects(data.other) }
+      </div>
+    </section>
+  </div>
 );
 
 export default Porfolio;
