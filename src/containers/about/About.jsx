c4cf9e1cac16059ef6d95ec96899f9a763b4eada
Louise Taylor <louise.taylor01@bbc.co.uk>
2019-01-10 13:59:33 +0000

    Update about

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { yearsFromNow } from '../../util/handy-things'
+
 import '../../styles/containers/about/about.css';
 
 import dancing from '../../assets/louise.jpg';
@@ -11,9 +13,11 @@ const About = () => (
             src={ dancing }
             alt="Longboard dancing in front of a corporate building."
         />
-        <p>I am currently employed as a Software Engineer at the BBC, with a particular interest in web accessibility. I've been honing my skills as a web developer for the past 2 years in industry and constantly learning in my spare time on personal projects.</p>
+        <p>I am a full stack developer with a particular interest in accessibility. Currently I work on BBC Sounds and before working at the BBC I worked at Technology Will Save Us.</p>
         <br />
-        <p>My hobbies include longboarding (or anything that involves a board). You can find me at competitions and UK/world wide events, I have <a href="http://spin-skate.com/events/girl-skate-session-in-sheffield-uk/" target="blank">written for S*pin Skate magazine</a> as well as <a href="https://thrillmagazine.com/boarders-without-borders/" target="blank">Thrill Magazine</a> and skated 200km around The Netherlands.</p>
+        <p>{ yearsFromNow(new Date('2017, July 18')) } I graduated from the University of Sheffield with a Masters degree in Physics.</p>
+        <br />
+        <p>When I am not working I am normally skateboarding. I am a pretty keen longboarder so regularly attend competitions and UK/world wide events, I've skated 200km around the Netherlands and I have written a couple of articles in longboarding magazines including <a href="http://spin-skate.com/events/girl-skate-session-in-sheffield-uk/" target="blank">S*pin Skate</a> and <a href="https://thrillmagazine.com/boarders-without-borders/" target="blank">Thrill</a>.</p>
     </div>
 );
 
---
+++ src/util/handy-things.js
@@ -0,0 +1,16 @@
+const yearsFromNow = (date) => {
+    const today = new Date();
+
+    let dMonths;
+    dMonths = (date.getFullYear() - today.getFullYear()) * 12;
+    dMonths -= today.getMonth() + 1;
+    dMonths += date.getMonth();
+    const dYears = dMonths / 12
+    const roundedYears = Math.round(Math.abs(dYears))
+
+    return `${roundedYears} year${roundedYears > 1 ? 's' : ''} ${dYears < 0 ? 'ago' : 'in the future'}`
+};
+
+export {
+    yearsFromNow
+};
