cc03f2029aa36d109724c17604ecde1b2a569c6a
Louise Taylor <louise.taylor01@bbc.co.uk>
2019-01-10 11:22:50 +0000

    Update job

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -5,7 +5,7 @@ import '../../styles/containers/home/home.css';
 const Home = () => (
     <div className="profile">
         <h1 className="profile__heading">Louise<br/>Taylor</h1>
-        <h2 className="profile__subheading">Full Stack Engineer at BBC</h2>
+        <h2 className="profile__subheading">Full Stack Engineer at BBC Sounds</h2>
     </div>
 );
 
