5c611d5e265924efad000c13b2464f494e54a9de
Louise Taylor <louise.taylor01@bbc.co.uk>
2019-01-23 22:16:40 +0000

    Emoji accessibility

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,19 +5,19 @@ import '../../styles/components/navbar/navbar.css';
 
 const Navbar = () => (
     <nav className="navbar">
-        <NavLink exact to="/" className="navbar__item" aria-label="Home">
+        <NavLink exact to="/" className="navbar__item">
             <span className="navbar__text">Home</span>
-            <span className="navbar__icon">🏠</span>
+            <span className="navbar__icon" role="img" aria-label="Home">🏠</span>
         </NavLink>
 
-        <NavLink to="/portfolio" className="navbar__item" aria-label="Portfolio">
+        <NavLink to="/portfolio" className="navbar__item">
             <span className="navbar__text">Portfolio</span>
-            <span className="navbar__icon">👩🏼‍💻</span>
+            <span className="navbar__icon" role="img" aria-label="Portfolio">👩🏼‍💻</span>
         </NavLink>
 
-        <NavLink to="/about" className="navbar__item" aria-label="About">
+        <NavLink to="/about" className="navbar__item">
             <span className="navbar__text">About</span>
-            <span className="navbar__icon">🙋🏼‍♀️</span>
+            <span className="navbar__icon" role="img" aria-label="About">🙋🏼‍♀️</span>
         </NavLink>
     </nav>
 );
