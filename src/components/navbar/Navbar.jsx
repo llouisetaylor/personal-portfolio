2a4c76b83eb9b40784d3fccdf2b2d22713053a07
Louise Taylor <louise.taylor01@bbc.co.uk>
2019-01-23 22:06:31 +0000

    Delete fontello

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import '../../styles/components/navbar/navbar.css';
-import '../../assets/fontello/css/fontello.css';
 
 const Navbar = () => (
     <nav className="navbar">
         <NavLink exact to="/" className="navbar__item" aria-label="Home">
             <span className="navbar__text">Home</span>
-            <i className="navbar__icon icon-home" />
+            <span className="navbar__icon">🏠</span>
         </NavLink>
 
         <NavLink to="/portfolio" className="navbar__item" aria-label="Portfolio">
             <span className="navbar__text">Portfolio</span>
-            <i className="navbar__icon icon-laptop" />
+            <span className="navbar__icon">👩🏼‍💻</span>
         </NavLink>
 
         <NavLink to="/about" className="navbar__item" aria-label="About">
             <span className="navbar__text">About</span>
-            <i className="navbar__icon icon-child" />
+            <span className="navbar__icon">🙋🏼‍♀️</span>
         </NavLink>
     </nav>
 );
