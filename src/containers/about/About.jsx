7e3b5029a80df950a3a8b7ccba9cbfb71be54690
Louise Taylor <louise.taylor01@bbc.co.uk>
2020-02-12 11:51:58 +0000

    add pic of louise

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import { yearsFromNow } from '../../util/handy-things'
 
 import '../../styles/containers/about/about.css';
+import louise from '../../assets/louise.png';
 
 const metaData = () => (
     <Helmet>
@@ -15,9 +16,12 @@ const metaData = () => (
 const About = () => (
     <div className="about">
         { metaData() }
-        <p>Hello, I am Louise and I am a software engineer. { yearsFromNow(new Date('2017/07/18')) } I graduated from the University of Sheffield with a Masters degree in Physics and since then I have been working as a web developer with a particular interest in accessibility.</p>
-        <br />
-        <p>I currently work at BBC Sounds, previously I have worked at <a href="https://make.techwillsaveus.com/" target="_blank" rel="noopener noreferrer">Technology Will Save Us</a> and BBC Internal Web Apps.</p>
+        <img src={ louise } alt="Illustration of Louise skateboarding" className="about__image" />
+        <div className="about__text">
+            <p>Hello, I am Louise and by profession I am a software engineer. { yearsFromNow(new Date('2017/07/18')) } I graduated from the University of Sheffield with a Masters degree in Physics and since then I have been working as a web developer with a particular interest in accessibility.</p>
+            <br />
+            <p>I currently work at BBC Sounds, previously I have worked at <a href="https://make.techwillsaveus.com/" target="_blank" rel="noopener noreferrer">Technology Will Save Us</a> and BBC Internal Web Apps.</p>
+        </div>
     </div>
 );
 
