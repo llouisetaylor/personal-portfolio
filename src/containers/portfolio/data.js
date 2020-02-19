62060a9cc7374a4e662dbf4bf02421273735e395
Louise Taylor <louise.taylor01@bbc.co.uk>
2020-02-19 11:16:29 +0000

    much better

--- src/containers/portfolio/data.js
+++ src/containers/portfolio/data.js
@@ -64,7 +64,7 @@ export default {
       url: "https://llouisetaylor.github.io/JSToys/bee-happy/",
       image: "https://llouisetaylor.s3.eu-west-2.amazonaws.com/beehappy.jpg",
       description: [
-        "A small project that determines if a text entry is happy, sad or unknown based on a predetermined set of keywords and displays this on a scale."
+        "Made for a job application - given brief to make a web app that determines if text is happy, sad or unknown - and decided to make a game out of it."
       ],
       technologies: ["jQuery", "VanillaJS"]
     }
